b2c3c7023ba87b3c49912fe557a56bd7b178c15f
AlexTorres <jalexandert@gmail.com>
2014-09-27 11:33:29 -0500

    Project Created

---
+++ Mapp/Mapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mapp
+//
+//  Created by John A Torres B on 27/09/14.
+//  Copyright (c) 2014 hangover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mapp/Mapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mapp
+//
+//  Created by John A Torres B on 27/09/14.
+//  Copyright (c) 2014 hangover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
